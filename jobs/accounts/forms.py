909905daa1f881925fef521af29a0fe8caac2e2c
DmitryKP <savemyvk@yandex.ru>
2022-03-23 20:08:28 +0300

    fin commit

---
+++ jobs/accounts/forms.py
@@ -0,0 +1,73 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from job_service.models import Application, Company, Vacancy
+from django.forms import ModelForm
+from crispy_forms.helper import FormHelper
+
+
+class UserRegistrationForm(UserCreationForm):
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email')
+
+    def __init__(self, *args, **kwargs):
+        super(UserRegistrationForm, self).__init__(*args, **kwargs)
+        self.fields['username'].label = "Логин"
+        self.fields['first_name'].label = "Имя"
+        self.fields['last_name'].label = "Фамилия"
+        self.fields['email'].label = "Электронная почта"
+        self.fields['password1'].label = "Пароль"
+        self.fields['password2'].label = "Подтверждение пароля"
+
+
+class UserLoginForm(forms.Form):
+    username = forms.CharField(max_length=255, required=True)
+    password = forms.CharField(widget=forms.PasswordInput)
+
+    model = User
+    fields = ['username', 'password']
+
+
+class ApplicationForm(ModelForm):
+
+    class Meta:
+        model = Application
+        fields = ('written_username', 'written_phone', 'written_cover_letter')
+
+
+class CompanyForm(ModelForm):
+
+    class Meta:
+        model = Company
+        fields = ('name', 'location', 'description', 'employee_count', 'logo')
+
+    def __init__(self, *args, **kwargs):
+        super(CompanyForm, self).__init__(*args, **kwargs)
+        self.fields['name'].label = "Имя"
+        self.fields['location'].label = "Локация"
+        self.fields['description'].label = "Описание"
+        self.fields['employee_count'].label = "Кол-во работников"
+        self.fields['logo'].label = "Логотип"
+
+        self.helper = FormHelper()
+        self.helper.form_tag = False
+
+
+class VacancyForm(ModelForm):
+
+    class Meta:
+        model = Vacancy
+        fields = ('title', 'salary_min', 'salary_max', 'specialty', 'skills', 'description')
+
+    def __init__(self, *args, **kwargs):
+        super(VacancyForm, self).__init__(*args, **kwargs)
+        self.fields['title'].label = "Название вакансии"
+        self.fields['skills'].label = "Требуемые навыки"
+        self.fields['salary_min'].label = "Зарплата от"
+        self.fields['salary_max'].label = "Зарплата до"
+        self.fields['specialty'].label = "Специализация"
+
+        self.helper = FormHelper()
+        self.helper.form_tag = False
